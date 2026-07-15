3a22344ac3c701cc6f59b0a6f2a0d9340fe4c9d8
Armando Rivas <rivasarmando271084@icloud.com>
2026-07-14 21:18:14 -0600

    Added initial project

---
+++ src/main/java/com/mesofi/myth_cloth_market_service/MythClothMarketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mesofi.myth_cloth_market_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MythClothMarketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MythClothMarketServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mesofi/myth_cloth_market_service/MythClothMarketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesofi.myth_cloth_market_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MythClothMarketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
